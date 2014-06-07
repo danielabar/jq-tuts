a96fb52e1bd61ef2c4372d98c4e7cbf2f3f12611
Daniela Baron <baron.daniela@gmail.com>
2014-06-07 19:11:52 -0400

    lesson 3-1 in progress

--- app/scripts/lesson3-1.js
+++ app/scripts/lesson3-1.js
@@ -2,4 +2,43 @@
 
 (function() {
 
+  // If user has JS enabled, this will add js class to html
+  // Then we can use this in css to hide the form (because we'll show it on js events)
+  $('html').addClass('js');
+
+  // Use an object to keep the code organized
+  // Within the context of an object, 'this' refers to the object itself
+  var contactForm = {
+
+    container: $('#contact'),
+
+    init: function() {
+      // create a button that displays the contact form
+      $('<button></button>', {
+        text: 'Contact Me',
+        class: 'btn btn-primary'
+      })
+        .insertAfter('article:first')
+        .on('click', this.show);
+    },
+
+    show: function() {
+      contactForm.close.call(contactForm.container);
+      contactForm.container.show();
+    },
+
+    close: function() {
+      var $this = $(this); // #contact
+      $('<span class="close glyphicon glyphicon-remove"></span>')
+        .prependTo(this)
+        .on('click', function() {
+          // this = span
+          $this.hide();
+        });
+    }
+
+  };
+
+  contactForm.init();
+
 })();
