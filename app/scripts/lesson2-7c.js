9b513488a651449ab3f0418e73f227174adc2ff4
Daniela Baron <baron.daniela@gmail.com>
2014-06-08 14:18:36 -0400

    complete dom appending exercises

--- app/scripts/lesson2-7c.js
+++ app/scripts/lesson2-7c.js
@@ -22,46 +22,43 @@
     $('h1').after('AFTER: THIS IS AFTER THE H1');
   });
 
-  // add text before the first paragraph
-  $('p').first().before('BEFORE: THIS IS BEFORE THE FIRST PARAGRAPH');
+  $('#addTextBeforeFirstPara').on('click', function() {
+    $('p').first().before('BEFORE: THIS IS BEFORE THE FIRST PARAGRAPH');
+  });
 
-  // OLD WAY to append html fragment - DO NOT DO THIS IF CAN BE AVOIDED
-  $('<h2>APPENDED H2 ELEMENT SLOPPY</h2>').appendTo('article');
+  $('#appendNewH2toArticle').on('click', function() {
+    $('<h2></h2', {
+      text: 'CREATED AN H2 WITH TEXT',
+      class: 'standout'
+    }).appendTo('article');
+  });
 
-  // A better way is to tell jQuery to create an h2,
-  // then pass content as argument
-  $('<h2></h2', {
-    text: 'CREATED AN H2 WITH TEXT AND CLASS, appendTo',
-    class: 'myClass'
-  }).appendTo('article');
+  $('#prependNewH2toArticle').on('click', function() {
+    $('<h2></h2', {
+      text: 'CREATED AN H2 WITH TEXT',
+      class: 'standout'
+    }).prependTo('article');
+  });
 
-  // Same technique can be used with prependTo
-  $('<h2></h2>', {
-    text: 'CREATED AN H2 WITH TEXT AND CLASS, prependTo',
-    class: 'myClass2'
-  }).prependTo('article');
+  $('#insertNewH2AfterH1').on('click', function() {
+    $('<h2></h2>', {
+      text: 'CREATED AN H2',
+      class: 'standout'
+    }).insertAfter('h1');
+  });
 
-  // Create an h2 and insert it after the existing h1 in the
-  $('<h2></h2>', {
-    text: 'CREATED AN H2 WITH TEXT AND CLASS, insertAfter',
-    class: 'myClass3'
-  }).insertAfter('article');
+  $('#insertNewH2BeforeSecondPara').on('click', function() {
+    $('<h2></h2', {
+      text: 'CREATED AN H2',
+      CLASS: 'standout'
+    }).insertBefore('p:eq(1)');
+  });
 
-  // Create an h2 and insert it before the second paragraph
-  $('<h2></h2', {
-    text: 'CREATED AN H2 insertBefore SECOND PARA',
-    CLASS: 'myClass4'
-  }).insertBefore('p:eq(1)');
+  $('#moveH1toBottom').on('click', function() {
+    $('#myPost').appendTo('article');
+  });
 
-  // Move existing h1 to bottom
-  $('#myPost').appendTo('article');
-
-  // Find the collection of paragraph tags, and after each, append the h1
-  $('p').after( $('h1') );
-
-  // More effective way to do above (with just first para tag)
-  // pass anon function to 'after', whatever it returns is what will be appended
-  $('p').eq(0).after(function() {
-    return $(this).prev();
+  $('#reset').on('click', function() {
+    location.reload();
   });
 })();
