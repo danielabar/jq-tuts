85d3cba5b59400ca86308b330898d109dd003257
Daniela Baron <baron.daniela@gmail.com>
2014-06-15 18:04:07 -0400

    lesson 4-2 handlebars helper function

--- app/scripts/lesson4-2.js
+++ app/scripts/lesson4-2.js
@@ -5,15 +5,21 @@
   // Handlebars automatically escapes html
   var data = [
     {
-      author: 'Jeffrey Way',
+      author: {first: 'Jeffrey', last: 'Way'},
       tweet: '<strong>30 Days</strong> to Learn jQuery Rocks',
       age: 27
     },
     {
-      author: 'Bugs Bunny',
-      tweet: 'What\'s up doc?'
+      author: {first: 'Bugs', last: 'Bunny'},
+      tweet: 'It\'s duck season!',
+      quote: 'What\'s up doc?'
     }
   ];
+
+  // Register a helper function to handle author object
+  Handlebars.registerHelper('fullName', function(author) {
+    return author.first + ' ' + author.last;
+  });
 
   // Compile the handlebars template
   var template = Handlebars.compile($('#template').html());
