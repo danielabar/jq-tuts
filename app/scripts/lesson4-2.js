3a3f333810f32d968b132d0e13e1fa99c8a864de
Daniela Baron <baron.daniela@gmail.com>
2014-06-15 17:40:12 -0400

    lesson 4-2 handlebars looping over arrays and conditionals

--- app/scripts/lesson4-2.js
+++ app/scripts/lesson4-2.js
@@ -2,26 +2,26 @@
 
 (function() {
 
-  var data1 = {
-    author: 'Jeffrey Way',
-    tweet: '30 Days to Learn jQuery Rocks'
-  };
-
-  var data2 = {
-    author: 'Bugs Bunny',
-    tweet: 'What\'s up doc?'
-  };
+  // Handlebars automatically escapes html
+  var data = [
+    {
+      author: 'Jeffrey Way',
+      tweet: '<strong>30 Days</strong> to Learn jQuery Rocks',
+      age: 27
+    },
+    {
+      author: 'Bugs Bunny',
+      tweet: 'What\'s up doc?'
+    }
+  ];
 
   // Compile the handlebars template
   var template = Handlebars.compile($('#template').html());
 
   // Bind data to the template
-  var temp = template(data1);
+  var temp = template(data);
 
   // Append the results of compiled/bound template to DOM
   $('ul.tweets').append(temp);
-
-  // Append more data
-  $('ul.tweets').append(template(data2));
 
 })();
