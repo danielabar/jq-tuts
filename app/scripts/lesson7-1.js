c59505442693187f591437c1647a582d09a21b37
Daniela Baron <baron.daniela@gmail.com>
2014-06-25 19:39:01 -0400

    lesson7-1 starting plugin development

---
+++ app/scripts/lesson7-1.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function() {
+
+  $('div.news').searchGuardian();
+
+})();
---
+++ app/scripts/searchGuardian.jquery.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$.fn.searchGuardian = function() {
+  console.log('this is the searchGuardian jQuery plugin');
+};
+
+  // var getNews = function(search) {
+  //   return $.ajax({
+  //     url: 'http://content.guardianapis.com/search?show-fields=all',
+  //     dataType: 'jsonp',
+  //     data: {q: search},
+  //   });
+  // };
